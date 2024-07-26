e30549cf680575306f1e4ed4c688c784109bdd4d
VISUARA <98740111+visuarafr@users.noreply.github.com>
2024-07-26 16:34:22 +0200

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,14 +1,18 @@
-// Configuration Firebase
-var firebaseConfig = {
-    apiKey: "AIzaSyCu5Ss3XxK9xhFxbsTiOUj6GBYJxS5MuiE",
-    authDomain: "monitoring-27236.firebaseapp.com",
-    projectId: "monitoring-27236",
-    storageBucket: "monitoring-27236.appspot.com",
-    messagingSenderId: "853833159557",
-    appId: "1:853833159557:web:8af061f2d3b5cf9f912aec"
-};
+// Vérifier si Firebase a déjà été initialisé pour éviter l'erreur "app/duplicate-app"
+if (!firebase.apps.length) {
+    // Configuration Firebase
+    var firebaseConfig = {
+        apiKey: "AIzaSyCu5Ss3XxK9xhFxbsTiOUj6GBYJxS5MuiE",
+        authDomain: "monitoring-27236.firebaseapp.com",
+        projectId: "monitoring-27236",
+        storageBucket: "monitoring-27236.appspot.com",
+        messagingSenderId: "853833159557",
+        appId: "1:853833159557:web:8af061f2d3b5cf9f912aec"
+    };
 
-// Initialize Firebase
-firebase.initializeApp(firebaseConfig);
+    // Initialize Firebase
+    firebase.initializeApp(firebaseConfig);
+}
+
 var auth = firebase.auth();
 var db = firebase.firestore();
