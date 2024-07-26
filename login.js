f3ee14213ffc61d6655c40b5fb093e779e5cb9d3
VISUARA <98740111+visuarafr@users.noreply.github.com>
2024-07-26 15:28:19 +0200

    Update login.js

--- login.js
+++ login.js
@@ -1,16 +1,5 @@
-// Firebase configuration
-var firebaseConfig = {
-    apiKey: "AIzaSyCu5Ss3XxK9xhFxbsTiOUj6GBYJxS5MuiE",
-    authDomain: "monitoring-27236.firebaseapp.com",
-    projectId: "monitoring-27236",
-    storageBucket: "monitoring-27236.appspot.com",
-    messagingSenderId: "853833159557",
-    appId: "1:853833159557:web:8af061f2d3b5cf9f912aec"
-};
 // Initialize Firebase
-firebase.initializeApp(firebaseConfig);
-
-var auth = firebase.auth();
+document.write('<script src="firebase-config.js"></script>');
 
 document.getElementById('loginBtn').addEventListener('click', function() {
     var email = document.getElementById('email').value;
