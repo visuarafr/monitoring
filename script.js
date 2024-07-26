74bf989769f037ab08a145265c6211dc62d5da3b
VISUARA <98740111+visuarafr@users.noreply.github.com>
2024-07-26 14:59:42 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,11 +1,11 @@
 // Firebase configuration
 var firebaseConfig = {
-    apiKey: "YOUR_API_KEY",
-    authDomain: "YOUR_AUTH_DOMAIN",
-    projectId: "YOUR_PROJECT_ID",
-    storageBucket: "YOUR_STORAGE_BUCKET",
-    messagingSenderId: "YOUR_MESSAGING_SENDER_ID",
-    appId: "YOUR_APP_ID"
+    apiKey: "AIzaSyCu5Ss3XxK9xhFxbsTiOUj6GBYJxS5MuiE",
+    authDomain: "monitoring-27236.firebaseapp.com",
+    projectId: "monitoring-27236",
+    storageBucket: "monitoring-27236.appspot.com",
+    messagingSenderId: "853833159557",
+    appId: "1:853833159557:web:8af061f2d3b5cf9f912aec"
 };
 // Initialize Firebase
 firebase.initializeApp(firebaseConfig);
