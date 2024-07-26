2d88e469d7fd7656219ec4a997e8225888a84253
VISUARA <98740111+visuarafr@users.noreply.github.com>
2024-07-26 16:28:05 +0200

    Update login.js

--- login.js
+++ login.js
@@ -1,12 +1,15 @@
 // Initialize Firebase
 document.write('<script src="firebase-config.js"></script>');
 
+// Login
 document.getElementById('loginBtn').addEventListener('click', function() {
-    var email = document.getElementById('email').value;
-    var password = document.getElementById('password').value;
-    auth.signInWithEmailAndPassword(email, password).then(user => {
-        window.location.href = "dashboard.html";
-    }).catch(error => {
-        console.error("Error signing in: ", error);
-    });
+    var email = document.getElementById('login-email').value;
+    var password = document.getElementById('login-password').value;
+    auth.signInWithEmailAndPassword(email, password)
+        .then(() => {
+            window.location.href = 'dashboard.html';
+        })
+        .catch(error => {
+            console.error('Error signing in: ', error);
+        });
 });
