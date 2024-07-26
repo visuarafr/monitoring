8db1da9e164e5ff9e4588ed2176e086d87fa45ee
VISUARA <98740111+visuarafr@users.noreply.github.com>
2024-07-26 17:56:23 +0200

    Create auth.js

---
+++ auth.js
@@ -0,0 +1,36 @@
+import { auth, db } from './firebase-config';
+
+// Création de compte
+const signup = async (email, password, role) => {
+  try {
+    const userCredential = await auth.createUserWithEmailAndPassword(email, password);
+    const user = userCredential.user;
+    await db.collection('users').doc(user.uid).set({
+      email: user.email,
+      role: role
+    });
+  } catch (error) {
+    console.error('Erreur de création de compte:', error);
+  }
+};
+
+// Connexion
+const login = async (email, password) => {
+  try {
+    await auth.signInWithEmailAndPassword(email, password);
+  } catch (error) {
+    console.error('Erreur de connexion:', error);
+  }
+};
+
+// Déconnexion
+const logout = async () => {
+  try {
+    await auth.signOut();
+  } catch (error) {
+    console.error('Erreur de déconnexion:', error);
+  }
+};
+
+// Export des fonctions d'authentification
+export { signup, login, logout };
