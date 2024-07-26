072d22dff375d486496db930e9ddb4a09878beb1
VISUARA <98740111+visuarafr@users.noreply.github.com>
2024-07-26 18:02:21 +0200

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,21 +1,20 @@
-// Vérifier si Firebase a déjà été initialisé pour éviter l'erreur "app/duplicate-app"
-if (!firebase.apps.length) {
-    // Configuration Firebase
-    var firebaseConfig = {
-        apiKey: "AIzaSyCu5Ss3XxK9xhFxbsTiOUj6GBYJxS5MuiE",
-        authDomain: "monitoring-27236.firebaseapp.com",
-        projectId: "monitoring-27236",
-        storageBucket: "monitoring-27236.appspot.com",
-        messagingSenderId: "853833159557",
-        appId: "1:853833159557:web:8af061f2d3b5cf9f912aec"
-    };
+import { initializeApp } from "https://www.gstatic.com/firebasejs/9.6.1/firebase-app.js";
+import { getAuth } from "https://www.gstatic.com/firebasejs/9.6.1/firebase-auth.js";
+import { getFirestore } from "https://www.gstatic.com/firebasejs/9.6.1/firebase-firestore.js";
 
-    // Initialize Firebase
-    firebase.initializeApp(firebaseConfig);
-}
+// Configuration de Firebase
+const firebaseConfig = {
+ apiKey: "AIzaSyCu5Ss3XxK9xhFxbsTiOUj6GBYJxS5MuiE",
+ authDomain: "monitoring-27236.firebaseapp.com",
+ projectId: "monitoring-27236",
+ storageBucket: "monitoring-27236.appspot.com",
+ messagingSenderId: "853833159557",
+ appId: "1:853833159557:web:8af061f2d3b5cf9f912aec"
+};
 
-var auth = firebase.auth();
-var db = firebase.firestore();
+// Initialisez Firebase
+const app = initializeApp(firebaseConfig);
+const auth = getAuth(app);
+const db = getFirestore(app);
 
-// Liste des emails des administrateurs
-const adminEmails = ["yannmartial@visuara.fr", "noe.labigne@visuara.fr"];
+export { auth, db };
